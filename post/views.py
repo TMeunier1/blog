f0ccbedbc55c15cdaa1ef3be6dbde5f83277e357
Tibau <xiliar1888@hotmail.fr>
2017-05-04 15:04:56 +0200

    Add two templates, and one post view

--- post/views.py
+++ post/views.py
@@ -4,3 +4,6 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    title = "Homepage"
+    return render(request, "homepage.html", { "title": title })
