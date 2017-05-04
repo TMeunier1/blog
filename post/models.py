f5139bd31fa98dc689998a1946598d2104e7f27c
ThibDT <thibault.de.tourdonnet@gmail.com>
2017-05-04 16:07:02 +0200

    add Post model and post in admin

--- post/models.py
+++ post/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.TextField(blank=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return "{0}".format(self.title)
