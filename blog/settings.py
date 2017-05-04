05198abca1a350ad2ed85f36763670ccc5b6f3b3
ThibDT <thibault.de.tourdonnet@gmail.com>
2017-05-04 15:20:52 +0200

    bugfix on post app

--- blog/settings.py
+++ blog/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'post',
 ]
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'blog', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- post/views.py
+++ post/views.py
@@ -5,5 +5,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    title = "Homepage"
-    return render(request, "homepage.html", { "title": title })
+    return render(request, "homepage.html")
