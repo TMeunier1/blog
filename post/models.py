189d5fb128ab12aa6e398d2169f42427003161dc
ThibDT <thibault.de.tourdonnet@gmail.com>
2017-05-11 13:40:13 +0200

    add django-autoslug to requirements.txt

--- post/models.py
+++ post/models.py
@@ -7,9 +7,9 @@ from autoslug import AutoSlugField
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(verbose_name="Titre", max_length=30)
+    slug = AutoSlugField(populate_from='title', editable=True)
     body = models.TextField(verbose_name="Contenu", blank=True)
     creation_date = models.DateTimeField(auto_now_add=True)
-    slug = AutoSlugField(populate_from='title')
 
     def __unicode__(self):
         return "{0}".format(self.title)
