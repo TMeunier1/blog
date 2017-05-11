13a6ef09be9727c2e664a13507c6a439cf8531a3
Tibau <xiliar1888@hotmail.fr>
2017-05-11 13:57:40 +0200

    Add categories

--- post/models.py
+++ post/models.py
@@ -5,11 +5,19 @@ from django.db import models
 from autoslug import AutoSlugField
 
 # Create your models here.
+class Category(models.Model):
+    label = models.CharField(verbose_name="Label", max_length=15)
+    def __unicode__(self):
+        return "{0}".format(self.label)
+    class Meta:
+        verbose_name = "Categorie"
+
 class Post(models.Model):
     title = models.CharField(verbose_name="Titre", max_length=30)
     slug = AutoSlugField(populate_from='title', editable=True)
     body = models.TextField(verbose_name="Contenu", blank=True)
     creation_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category)
 
     def __unicode__(self):
         return "{0}".format(self.title)
